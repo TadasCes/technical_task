4623e081300b0a7b1c81c67262ad9bb7c6548a58
VilkelisV <v.vilkelis97@gmail.com>
2020-07-02 10:36:53 +0300

    Init

---
+++ Vainius_Vilkelis_technical/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Vainius_Vilkelis_technical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            for (int i = 1; i <= h; i++)
+            {
+                for (int j = 1; j <= h - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int o = 1; o <= i; o++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
